71ce8b1025491a8ea8af8755b158438a3fe9a159
Daniel Dąbrowski <elosik123@onet.pl>
2019-01-24 22:44:36 +0100

    rwd for chat

--- client/src/components/chat/friends/friends.jsx
+++ client/src/components/chat/friends/friends.jsx
@@ -5,15 +5,32 @@ import FriendsItem from './friendsItem/friendsItem';
 import './friends.css';
 
 class Friends extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      friendsMenuShow: false
+    }
+  }
+
+  showFriendsMenu = () => {
+    this.setState({ friendsMenuShow: !this.state.friendsMenuShow });
+  }
+
   render() {
     return (
-        <div className="friends">
+        <>
+          <button onClick={this.showFriendsMenu} className={this.state.friendsMenuShow ? "friends__button--mobile active" : "friends__button--mobile"}>
+            <span class="fa fa-arrow-right"></span>
+          </button>
+          <div className={this.state.friendsMenuShow ? "friends active" : "friends"}>
             <SearchFriends />
             <FriendsItem userNick="Anfisa Novikova" userLastMessage="I really like the colour but can you change it to be..." userLastMessageTime="11:39" userAvatar="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397_960_720.png" />
             <FriendsItem userNick="Kent Smith" userLastMessage="Use what happens naturally." userLastMessageTime="10:15" userAvatar="https://www.w3schools.com/howto/img_avatar.png" />
             <FriendsItem active={true} userNick="Irene Williams" userLastMessage="Is this the best we can do?" userLastMessageTime="09:52" userAvatar="https://upload.wikimedia.org/wikipedia/commons/c/c9/Avatar_2_Placeholder_DumDarac.png" />
             <FriendsItem userNick="Dana Medina" userLastMessage="In an ideal world that sandwitch" userLastMessageTime="Yesterday" userAvatar="http://eliteadmin.themedesigner.in/demos/bt4/assets/images/users/1.jpg" />
-        </div>
+          </div>
+        </>
     );
   }
 }
--- client/src/components/chat/messanger/messangerChat/messangerChat.jsx
+++ client/src/components/chat/messanger/messangerChat/messangerChat.jsx
@@ -5,10 +5,23 @@ import MessageFriend from './messageFriend/messageFriend';
 import './messangerChat.css';
 
 class messangerChat extends Component {
+  componentDidMount() {
+    let chatElement = document.getElementById('messanger-chat');
+    chatElement.scrollTop = chatElement.scrollHeight;
+  }
+
   render() {
     return (
-        <div className="messanger__chat">
+        <div id="messanger-chat" className="messanger__chat">
           <MessageFriend text="siema eniu ziomeczku mójsiema eniu ziomeczku mójsiema eniu ziomeczku mójsiema eniu ziomeczku mójsiema eniu ziomeczku mójsiema eniu ziomeczku mójsiema eniu ziomeczku mójsiema eniu ziomeczku mój" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDDwefewfwefDDDDD" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDDDDDDD" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDwefwefDDDDDD" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDwefwefwefDDDDDD" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDDDDDDD" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDDDDDDD" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDDDDDDD" />
+          <MessageMe text="XDDDDDDDDDDDDDDDDDDDDDDDDD" />
           <MessageMe text="XDDDDDDDDDDDDDDDDDDDDDDDDD" />
         </div>
     );
